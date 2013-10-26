0ee94fc52f108c0db17709d9f1e624723b42347c
Klaus Hebsgaard <klaus@hebsgaard.dk>
2013-10-26 19:55:09 +0200

    init

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,288 @@
+
+function removeValueFromArray(arr, val){
+  var index = arr.indexOf(val);
+
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+function getValidsForColumn(column){
+  var valids = [1,2,3,4,5,6,7,8,9];
+  var as = _.map(valids, function(num) { return column + num; });
+  
+  _.each(as, function(entry){
+    var val = $("#" + entry + " input[type=number]").val();
+    valids = removeValueFromArray(valids, parseInt(val,10));
+  });
+  
+  return valids;
+}
+
+function getValidsForRow(row){
+  var valids = [1,2,3,4,5,6,7,8,9];
+  var columns = ["a","b","c","d","e","f","g","h","i"];
+  var as = _.map(columns, function(col) { return col + row; });
+  
+  _.each(as, function(entry){
+    var val = $("#" + entry + " input[type=number]").val();
+    valids = removeValueFromArray(valids, parseInt(val,10));
+  });
+  
+  return valids;
+}
+
+function getValidsForThreeByThree(startCols, startRows){
+  var valids = [1,2,3,4,5,6,7,8,9];
+  
+  var fields = _.map(startCols, function(col) { 
+    return _.map(startRows, function(row) { return col + row; }); 
+  });
+  _.each(fields, function(entry){
+   
+    _.each(entry, function(itm){
+    var val = $("#" + itm + " input[type=number]").val();
+
+    valids = removeValueFromArray(valids, parseInt(val,10));
+    });
+    
+  });
+
+  return valids;  
+}
+
+function isIn(row, column, arr){
+  return $.inArray(row, arr) !== -1 && $.inArray(column, arr) !== -1;
+}
+
+function getValidsForThreeByThreeAsString(row, column){
+  var a1 = ["a","b","c",1,2,3];
+  var d1 = ["d","e", "f", 1,2,3];
+  var g1= ["g","h","i",1,2,3];
+  var a4 = ["a","b","c",4,5,6];
+  var d4 = ["d","e","f",4,5,6];
+  var g4 = ["g","h","i",4,5,6];
+  var a7 = ["a","b","c",7,8,9];
+  var d7 = ["d","e","f",7,8,9];
+  var g7 = ["g","h","i",7,8,9];
+  
+  if(isIn(row, column, a1)){
+    return $("#sa1").text();
+  }
+  if(isIn(row, column, d1)){
+    return $("#sd1").text();
+  }
+  if(isIn(row, column, g1)){
+    return $("#sg1").text();
+  }
+  if(isIn(row, column, a4)){
+    return $("#sa4").text();
+  }
+  if(isIn(row, column, d4)){
+    return $("#sd4").text();
+  }
+  if(isIn(row, column, g4)){
+    return $("#sg4").text();
+  }
+  if(isIn(row, column, a7)){
+    return $("#sa7").text();
+  }
+  if(isIn(row, column, d7)){
+    return $("#sd7").text();
+  }
+  if(isIn(row, column, g7)){
+    return $("#sg7").text();
+  }
+}
+
+function removeCommaEntry(array){
+  var index = array.indexOf(",");
+  if (index > -1) {
+    array.splice(index, 1);
+    return array;
+}
+
+}
+
+function solve(){
+  $("#a").text(getValidsForColumn("a"));
+  $("#b").text(getValidsForColumn("b"));
+  $("#c").text(getValidsForColumn("c"));
+  $("#d").text(getValidsForColumn("d"));
+  $("#e").text(getValidsForColumn("e"));
+  $("#f").text(getValidsForColumn("f"));
+  $("#g").text(getValidsForColumn("g"));
+  $("#h").text(getValidsForColumn("h"));
+  $("#i").text(getValidsForColumn("i"));
+  
+  $("#1").text(getValidsForRow("1"));
+  $("#2").text(getValidsForRow("2"));
+  $("#3").text(getValidsForRow("3"));
+  $("#4").text(getValidsForRow("4"));
+  $("#5").text(getValidsForRow("5"));
+  $("#6").text(getValidsForRow("6"));
+  $("#7").text(getValidsForRow("7"));
+  $("#8").text(getValidsForRow("8"));
+  $("#9").text(getValidsForRow("9"));
+  
+  
+  $("#sa1").text(getValidsForThreeByThree(["a","b","c"], [1,2,3]));
+  $("#sd1").text(getValidsForThreeByThree(["d","e","f"], [1,2,3]));
+  $("#sg1").text(getValidsForThreeByThree(["g","h","i"], [1,2,3]));
+  $("#sa4").text(getValidsForThreeByThree(["a","b","c"], [4,5,6]));
+  $("#sd4").text(getValidsForThreeByThree(["d","e","f"], [4,5,6]));
+  $("#sg4").text(getValidsForThreeByThree(["g","h","i"], [4,5,6]));
+  $("#sa7").text(getValidsForThreeByThree(["a","b","c"], [7,8,9]));
+  $("#sd7").text(getValidsForThreeByThree(["d","e","f"], [7,8,9]));
+  $("#sg7").text(getValidsForThreeByThree(["g","h","i"], [7,8,9]));
+  
+  
+  var rows = [1,2,3,4,5,6,7,8,9];
+  var columns = ["a","b","c","d","e","f","g","h","i"];
+  var solution = {};
+  _.each(rows, function(row){
+    var validRowValues = $("#" + row).text();
+    _.each(columns, function(column){
+      var validColumnValues = $("#" + column).text();
+      console.log(column+row);
+      var val = $("#" + column+row + " input[type=number]").val();
+
+      if(!val){
+        tbtValids = getValidsForThreeByThreeAsString(row, column);
+        tbtValids = tbtValids.split(",");
+        validColumnValues = validColumnValues.split(",");
+       validValues = _.intersection(validColumnValues, validRowValues, tbtValids);
+        console.log(validValues);
+        solution[column+row] = validValues;
+        if(validValues.length === 1){
+          $("#" + column+row + " input[type=number]").val(validValues[0]);
+          
+        }
+      }
+    });
+  });
+  console.log(solution);
+  return solution;
+}
+
+function hasBeenSolved(solution){
+  var solved = true;
+  for (var key in solution) {
+     if(key.length!==0){
+       solved=false;
+     }
+   }
+   return solved;
+}
+$( document ).ready(function() {
+$("#reset").click(function(){
+  $("input[type=number]").val("");
+});
+
+$("#solve").on( "click", function(){
+  console.clear();
+  
+  var solution = {};
+  var lastSolution = {};
+  for(var i = 0;i < 20;i++){
+    if(solution === lastSolution){
+      alert("problem");
+    }
+    
+     solution = solve();
+    
+    if(hasBeenSolved(solution)){
+       break; 
+    }
+    lastSolution = solution;
+  }
+});
+
+
+$("#fill").on( "click", function(){
+  $("#a1 input[type=number]").val("2");
+  $("#c1 input[type=number]").val("8");
+  $("#d1 input[type=number]").val("7");
+  $("#g1 input[type=number]").val("9");
+  
+  $("#a3 input[type=number]").val("7");
+  $("#c3 input[type=number]").val("9");
+  $("#e3 input[type=number]").val("1");
+  $("#f3 input[type=number]").val("5");
+  $("#i3 input[type=number]").val("4");
+  
+  $("#g4 input[type=number]").val("1");
+  $("#i4 input[type=number]").val("3");
+  
+  $("#a5 input[type=number]").val("1");
+  $("#c5 input[type=number]").val("7");
+  $("#d5 input[type=number]").val("8");
+  $("#f5 input[type=number]").val("2");
+  $("#h5 input[type=number]").val("6");
+  
+  $("#c6 input[type=number]").val("5");
+  $("#h6 input[type=number]").val("2");
+  $("#i6 input[type=number]").val("7");
+  
+  $("#b7 input[type=number]").val("9");
+  $("#c7 input[type=number]").val("1");
+  
+  $("#b8 input[type=number]").val("2");
+  $("#g8 input[type=number]").val("6");
+  $("#h8 input[type=number]").val("3");
+  
+  $("#c9 input[type=number]").val("3");
+  $("#i9 input[type=number]").val("1");
+});
+
+$("#fill1").on( "click", function(){
+  
+  $("#b1 input[type=number]").val("6");
+  $("#e1 input[type=number]").val("4");
+  $("#f1 input[type=number]").val("3");
+  $("#i1 input[type=number]").val("2");
+  
+  $("#b2 input[type=number]").val("1");
+  $("#c2 input[type=number]").val("7");
+  $("#d2 input[type=number]").val("9");
+  $("#f2 input[type=number]").val("2");
+  $("#g2 input[type=number]").val("6");
+  $("#i2 input[type=number]").val("4");
+  
+  $("#c3 input[type=number]").val("4");
+  $("#e3 input[type=number]").val("8");
+  $("#h3 input[type=number]").val("3");
+  
+  $("#a4 input[type=number]").val("8");
+  $("#c4 input[type=number]").val("5");
+  $("#e4 input[type=number]").val("1");
+  $("#f4 input[type=number]").val("6");
+  $("#g4 input[type=number]").val("4");
+  
+  $("#d5 input[type=number]").val("8");
+  $("#e5 input[type=number]").val("9");
+  $("#g5 input[type=number]").val("2");
+  $("#i5 input[type=number]").val("1");
+  
+  $("#a6 input[type=number]").val("2");
+  $("#b6 input[type=number]").val("4");
+  $("#d6 input[type=number]").val("5");
+  $("#h6 input[type=number]").val("6");
+  
+  $("#b7 input[type=number]").val("3");
+  $("#c7 input[type=number]").val("9");
+  $("#d7 input[type=number]").val("4");
+  $("#h7 input[type=number]").val("2");
+  
+  $("#b8 input[type=number]").val("8");
+  $("#d8 input[type=number]").val("1");
+  $("#f8 input[type=number]").val("5");
+  $("#g8 input[type=number]").val("3");
+  
+  $("#c9 input[type=number]").val("6");
+  $("#f9 input[type=number]").val("9");
+  $("#h9 input[type=number]").val("1");
+  $("#i9 input[type=number]").val("7");
+});
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SodukoSolver::Application.config.secret_token = '7e8aadfe860160966f2d7889527b7e959d0d679917465b367263ecbc04fbfb665aeedb5c52e50e950239af043a845302335727aade0f669bd1f95778505bda0a'
